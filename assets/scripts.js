e4b5091e81e5d615ae664f1593cbcf36ab957f30
Anant S <startanant@gmail.com>
2020-02-10 04:24:21 -0500

    Save scores to local storage

--- assets/scripts.js
+++ assets/scripts.js
@@ -40,6 +40,8 @@ const myQuestions = [
 const quizContainer = document.getElementById('quiz');
 const resultsContainer = document.getElementById('results');
 const submitButton = document.getElementById('submit');
+var numCorrect = 0;
+var subtract = 0;
 
 function buildQuiz(){
     // variable to store the HTML output
@@ -90,59 +92,104 @@ function buildQuiz(){
     quizContainer.innerHTML = output.join('');
 }
 
-function showResults(){
+// function showResults(){
 
-    // gather answer containers from our quiz
-    const answerContainers = quizContainer.querySelectorAll('.answers');
+//     // gather answer containers from our quiz
+//     const answerContainers = quizContainer.querySelectorAll('.answers');
   
-    // keep track of user's answers
-    let numCorrect = 0;
+//     // keep track of user's answers
+//     //let numCorrect = 0;
   
-    // for each question...
-    myQuestions.forEach( (currentQuestion, questionNumber) => {
+//     // for each question...
+//     myQuestions.forEach( (currentQuestion, questionNumber) => {
   
-      // find selected answer
-      const answerContainer = answerContainers[questionNumber];
-      const selector = `input[name=question${questionNumber}]:checked`;
-      const userAnswer = (answerContainer.querySelector(selector) || {}).value;
+//       // find selected answer
+//       const answerContainer = answerContainers[questionNumber];
+//       const selector = `input[name=question${questionNumber}]:checked`;
+//       const userAnswer = (answerContainer.querySelector(selector) || {}).value;
   
-      // if answer is correct
-      if(userAnswer === currentQuestion.correctAnswer){
-        // add to the number of correct answers
+//       // if answer is correct
+//       if(userAnswer === currentQuestion.correctAnswer){
+//         // add to the number of correct answers
+//         numCorrect++;
+  
+//         // color the answers green
+//         answerContainers[questionNumber].style.color = 'lightgreen';
+//       }
+//       // if answer is wrong or blank
+//       else{
+//         // color the answers red
+//         answerContainers[questionNumber].style.color = 'red';
+//       }
+//     });
+  
+//     // show number of correct answers out of total
+//     resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+// }
+
+function verifyAnswer()
+{
+    //alert('verifying answer');
+    currentQuestionNumber = this.name.substring(8);
+    currentQuestion = myQuestions[currentQuestionNumber];
+    userAnswer = this.value;
+    if (userAnswer == currentQuestion.correctAnswer) {
+        //alert('correct answer');
         numCorrect++;
-  
-        // color the answers green
-        answerContainers[questionNumber].style.color = 'lightgreen';
-      }
-      // if answer is wrong or blank
-      else{
-        // color the answers red
-        answerContainers[questionNumber].style.color = 'red';
-      }
-    });
-  
-    // show number of correct answers out of total
-    resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
+        //alert("score "+numCorrect);
+    } else {
+        //alert('wrong answer');
+        subtractTime();
+    }
+
+    showNextSlide();
 }
 
 function showSlide(n) {
-    slides[currentSlide].classList.remove('active-slide');
-    slides[n].classList.add('active-slide');
-    currentSlide = n;
-    if(currentSlide === 0){
-      previousButton.style.display = 'none';
+    if (n == 999) {
+        slides[currentSlide].classList.remove('active-slide');
+
+        quizContainer.innerHTML = `Enter your name. 
+        Your score is ${numCorrect} 
+        <input type="text" id="name"><button onclick="saveScore()">Save</button>`;
+        
+    } else {
+        slides[currentSlide].classList.remove('active-slide');
+        slides[n].classList.add('active-slide');
+        currentSlide = n;
+        if(currentSlide === 0){
+        previousButton.style.display = 'none';
+        }
+        else{
+        previousButton.style.display = 'inline-block';
+        }
+        if(currentSlide === slides.length-1){
+        nextButton.style.display = 'none';
+        submitButton.style.display = 'inline-block';
+        showScore();
+        }
+        else{
+        nextButton.style.display = 'inline-block';
+        submitButton.style.display = 'none';
+        }
     }
-    else{
-      previousButton.style.display = 'inline-block';
-    }
-    if(currentSlide === slides.length-1){
-      nextButton.style.display = 'none';
-      submitButton.style.display = 'inline-block';
-    }
-    else{
-      nextButton.style.display = 'inline-block';
-      submitButton.style.display = 'none';
-    }
+    
+}
+
+function saveScore() {
+    var name = document.getElementById("name").value;
+    let scoreList = localStorage.scoreList ? JSON.parse( localStorage.scoreList ) : [];
+    scoreList.push(name+" "+numCorrect);
+    localStorage.scoreList = JSON.stringify(scoreList);
+    displayScores();
+}
+
+function displayScores() {
+    let scoreList = localStorage.scoreList ? JSON.parse( localStorage.scoreList ) : [];
+    quizContainer.innerHTML = '<b>Scores<b> <br>';
+    scoreList.forEach(score => {
+        quizContainer.innerHTML += `${score} <br>`;
+    });
 }
 
 function showNextSlide() {
@@ -153,29 +200,42 @@ function showPreviousSlide() {
     showSlide(currentSlide - 1);
 }
 
+function showScore() {
+    showSlide(999);
+}
+
 // display quiz right away
 buildQuiz();
 
 // Pagination
 const previousButton = document.getElementById("previous");
 const nextButton = document.getElementById("next");
+const hiScore = document.getElementById("hi-score");
+//hiScore.style.visibility = 'hidden';
 const slides = document.querySelectorAll(".slide");
 let currentSlide = 0;
 
 // on submit, show results
-submitButton.addEventListener('click', showResults);
+//submitButton.addEventListener('click', showResults);
 previousButton.addEventListener("click", showPreviousSlide);
 nextButton.addEventListener("click", showNextSlide);
 
 
 document.querySelectorAll('.radio').forEach(radio => {
-    radio.addEventListener('click', showNextSlide);
+    radio.addEventListener('click', verifyAnswer);
 });
 
 //timer
-var timeInMinutes = 1;
+//var timeInMinutes = 0;
+var timeInSeconds = 20;
 var currentTime = Date.parse(new Date());
-var deadline = new Date(currentTime + timeInMinutes*60*1000);
+//var deadline = new Date(currentTime + timeInMinutes*60*1000);
+var deadline = new Date(currentTime + timeInSeconds*1000);
+
+function subtractTime()
+{
+    subtract = 5*1000;
+}
 
 function getTimeRemaining(endtime){
     var t = Date.parse(endtime) - Date.parse(new Date());
@@ -194,11 +254,19 @@ function getTimeRemaining(endtime){
 function initializeClock(id, endtime){
     var clock = document.getElementById(id);
     var timeinterval = setInterval(function(){
+      console.log(subtract);
+      if(subtract>0){
+        endtime = Date.parse(endtime) - subtract;
+        endtime = new Date(endtime);
+        subtract = 0;
+      }
       var t = getTimeRemaining(endtime);
       clock.innerHTML = 'Time remaining: ' + t.minutes  +
                         ': ' + t.seconds;
       if(t.total<=0){
+        alert("time up");
         clearInterval(timeinterval);
+        showScore();
       }
     },1000);
 }
