035280e7aef09f38f789e04a862e660410b69a31
Anant S <startanant@gmail.com>
2020-02-10 00:50:58 -0500

    Add timer

--- assets/scripts.js
+++ assets/scripts.js
@@ -171,3 +171,36 @@ nextButton.addEventListener("click", showNextSlide);
 document.querySelectorAll('.radio').forEach(radio => {
     radio.addEventListener('click', showNextSlide);
 });
+
+//timer
+var timeInMinutes = 1;
+var currentTime = Date.parse(new Date());
+var deadline = new Date(currentTime + timeInMinutes*60*1000);
+
+function getTimeRemaining(endtime){
+    var t = Date.parse(endtime) - Date.parse(new Date());
+    var seconds = Math.floor( (t/1000) % 60 );
+    var minutes = Math.floor( (t/1000/60) % 60 );
+    var hours = Math.floor( (t/(1000*60*60)) % 24 );
+    var days = Math.floor( t/(1000*60*60*24) );
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+}
+function initializeClock(id, endtime){
+    var clock = document.getElementById(id);
+    var timeinterval = setInterval(function(){
+      var t = getTimeRemaining(endtime);
+      clock.innerHTML = 'Time remaining: ' + t.minutes  +
+                        ': ' + t.seconds;
+      if(t.total<=0){
+        clearInterval(timeinterval);
+      }
+    },1000);
+}
+initializeClock('clockdiv', deadline);
+
