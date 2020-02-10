ae3c3750c48e65af3e968d3af9cf43ab35dee39f
Anant S <startanant@gmail.com>
2020-02-10 00:31:12 -0500

    Add event listener on radio buttons to show next question

--- assets/scripts.js
+++ assets/scripts.js
@@ -59,7 +59,7 @@ function buildQuiz(){
           // ...add an HTML radio button
           answers.push(
             `<label>
-              <input type="radio" name="question${questionNumber}" value="${letter}">
+              <input class="radio" type="radio" name="question${questionNumber}" value="${letter}">
               ${letter} :
               ${currentQuestion.answers[letter]}
             </label>`
@@ -67,10 +67,22 @@ function buildQuiz(){
         }
   
         // add this question and its answers to the output
-        output.push(
-          `<div class="question"> ${currentQuestion.question} </div>
-          <div class="answers"> ${answers.join('')} </div>`
-        );
+        if(questionNumber == 0) {
+            output.push(
+                `<div class="slide active-slide">
+                  <div class="question"> ${currentQuestion.question} </div>
+                  <div class="answers"> ${answers.join("")} </div>
+                </div>`
+              );
+        } else {
+            output.push(
+                `<div class="slide">
+                  <div class="question"> ${currentQuestion.question} </div>
+                  <div class="answers"> ${answers.join("")} </div>
+                </div>`
+              );
+        }
+        
       }
     );
   
@@ -112,9 +124,50 @@ function showResults(){
     // show number of correct answers out of total
     resultsContainer.innerHTML = `${numCorrect} out of ${myQuestions.length}`;
 }
+
+function showSlide(n) {
+    slides[currentSlide].classList.remove('active-slide');
+    slides[n].classList.add('active-slide');
+    currentSlide = n;
+    if(currentSlide === 0){
+      previousButton.style.display = 'none';
+    }
+    else{
+      previousButton.style.display = 'inline-block';
+    }
+    if(currentSlide === slides.length-1){
+      nextButton.style.display = 'none';
+      submitButton.style.display = 'inline-block';
+    }
+    else{
+      nextButton.style.display = 'inline-block';
+      submitButton.style.display = 'none';
+    }
+}
+
+function showNextSlide() {
+    showSlide(currentSlide + 1);
+}
   
+function showPreviousSlide() {
+    showSlide(currentSlide - 1);
+}
+
 // display quiz right away
 buildQuiz();
 
+// Pagination
+const previousButton = document.getElementById("previous");
+const nextButton = document.getElementById("next");
+const slides = document.querySelectorAll(".slide");
+let currentSlide = 0;
+
 // on submit, show results
 submitButton.addEventListener('click', showResults);
+previousButton.addEventListener("click", showPreviousSlide);
+nextButton.addEventListener("click", showNextSlide);
+
+
+document.querySelectorAll('.radio').forEach(radio => {
+    radio.addEventListener('click', showNextSlide);
+});
